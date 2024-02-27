78c1e170a75bdd6d8aaba7caa6bfe351f5e5725a
AndreyJz <jerez.01.18@gmail.com>
2024-02-27 18:04:20 -0500

    Menus listos

--- modules/menus.py
+++ modules/menus.py
@@ -12,6 +12,92 @@ def mainmenu():
     print(opciones)
     op = input('Ingrese el numero de la seccion a la que quiere ingresar <-> ')
     if (op==1):
+        opcion='ACTIVOS'
+        menuAPZ(opcion)
+    elif (op==2):
+        opcion='PERSONAL'
+        menuAPZ(opcion)
+    elif (op==3):
+        opcion='ZONAS'
+        menuAPZ(opcion)
+    elif (op==4):
+        menuAsigActivos()
+    elif (op==5):
+        menuRep()
+    elif (op==6):
+        menuMOVActivos()
+    elif (op==7):
+        print('Gracias por usar el programa, vuelva pronto! :D')
+        os.system('pause')
+    else:
+        print('El valor ingresado no esta asociado a una seccion...')
+        os.system('pause')
+        mainmenu()
+
+def menuAPZ(opcion):
+    titulo = f"""
+    +++++++++++++++++
+    [ MENU {opcion} ]
+    +++++++++++++++++
+    """
+    print(titulo)
+    lstOp=[1,2,3,4,5]
+    opciones = '1. Agregar\n2. Editar\n3. Eliminar\n4. Buscar\n5. Regresar al Menu Principal\n'
+    print(opciones)
+    op = input('Ingrese el numero de la seccion a la que quiere ingresar <-> ')
+    if (op==1):
+        pass
+    if (op==2):
+        pass
+    if (op==3):
+        pass
+    if (op==4):
+        pass
+    if (op==5):
+        print('Volviendo al menu principal...')
+        os.system('pause')
+        mainmenu()
+    else:
+        print('El valor ingresado no esta asociado a una seccion...')
+        os.system('pause')
+        menuAPZ(opcion)
+
+def menuAsigActivos():
+    titulo = """
+    ++++++++++++++++++++++++++++++
+    [ MENU ASIGNACION DE ACTIVOS ]
+    ++++++++++++++++++++++++++++++
+    """
+    print(titulo)
+    lstOp=[1,2,3]
+    opciones = '1. Crear Asignacion\n2. Buscar Asignacion\n3. Regresar al Menu Principal\n'
+    print(opciones)
+    op = input('Ingrese el numero de la seccion a la que quiere ingresar <-> ')
+    if (op==1):
+        pass
+    elif(op==2):
+        pass
+    elif(op==3):
+        print('Volviendo al menu principal...')
+        os.system('pause')
+        mainmenu()
+    else:
+        print('El valor ingresado no esta asociado a una seccion...')
+        os.system('pause')
+        menuAsigActivos()
+
+def menuRep():
+    titulo = """
+    +++++++++++++++++
+    [ MENU REPORTES ]
+    +++++++++++++++++
+    """
+    print(titulo)
+    lstOp=[1,2,3,4,5,6,7]
+    opciones = '1. Lista Activos\n2. Lista Activos por Categoria\n3. Lista Activos dados de Baja\n4. Lista Activos y Asignacion\n5. Lista Historial de Mov. de Activo\n6. Movimiento De Activos\n7. Regresar al Menu Principal\n'
+    print(opciones)
+    op = input('Ingrese el numero de la seccion a la que quiere ingresar <-> ')
+    if (op==1):
         pass
     elif (op==2):
         pass
@@ -24,21 +110,38 @@ def mainmenu():
     elif (op==6):
         pass
     elif (op==7):
-        print('Gracias por usar el programa, vuelva pronto! :D')
-        os.system('pause')
-    else:
-        print('El valor ingresado no esta asociado a unaa seccion...')
+        print('Volviendo al menu principal...')
         os.system('pause')
         mainmenu()
+    else:
+        print('El valor ingresado no esta asociado a una seccion...')
+        os.system('pause')
+        menuRep()
 
-def menuAPZ(opcion):
-    titulo = f"""
-    +++++++++++++++++
-    + MENU {opcion} +
-    +++++++++++++++++
+def menuMOVActivos():
+    titulo = """
+    ++++++++++++++++++++++++++++++
+    [ MENU MOVIMIENTO DE ACTIVOS ]
+    ++++++++++++++++++++++++++++++
     """
     print(titulo)
     lstOp=[1,2,3,4,5]
-    opciones = '1. Agregar\n2. Editar\n3. Eliminar\n4. Buscar\n5. Salir'
+    opciones = '1. Retorno de activo\n2. Dar de Baja Activo\n3. Cambiar Asignacion de Activo\n4. Envia a Garantia Activo\n5. Regresar al Menu Principal\n'
     print(opciones)
     op = input('Ingrese el numero de la seccion a la que quiere ingresar <-> ')
+    if (op==1):
+        pass
+    elif(op==2):
+        pass
+    elif(op==3):
+        pass
+    elif(op==4):
+        pass
+    elif(op==5):
+        print('Volviendo al menu principal...')
+        os.system('pause')
+        mainmenu()
+    else:
+        print('El valor ingresado no esta asociado a una seccion...')
+        os.system('pause')
+        menuMOVActivos()
