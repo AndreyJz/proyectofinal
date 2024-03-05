bea418c2359641425d844be2d4e80a8d973ab509
AndreyJz <jerez.01.18@gmail.com>
2024-03-04 20:55:16 -0500

    try para Crtl C

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from corefiles import checkFile, updateData
+from corefiles import checkFile, updateData, pausar_pantalla, borrar_pantalla
 from modules.menus import mainmenu
 from externo.csv_import import AddActivoFromCampus
 inventario = {
@@ -12,4 +12,13 @@ data = checkFile('data.json', inventario)
 #updateData('data.json', inventario)
 
 if __name__ == '__main__':
-    mainmenu(data)
+    while True:
+        try:
+            mainmenu(data)
+        except KeyboardInterrupt:
+            borrar_pantalla()
+            print('Recuerde que el buen comportamiento del usuario es escencial para que el codigo funcione de manera adecuada, NO rompa el codigo')
+            pausar_pantalla()
+        else:
+            mainmenu(data)
+            break
